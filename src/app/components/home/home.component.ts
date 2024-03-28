9829fba6d7c497a2465a670ebba6a42dbe4f667f
lucaslazzari <lucaslazzari1812@gmail.com>
2024-03-28 17:22:37 -0300

    subindo projeto completo

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: 
+  [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    NgOptimizedImage
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
